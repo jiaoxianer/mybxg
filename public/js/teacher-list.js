d100647d44b8c98c66ed13269aa6ddb21affa7d5
tom <tom@itcast>
2017-09-21 22:27:52 +0800

    讲师预览功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,18 +1,18 @@
-define(['jquery','template'],function($,template){
+define(['jquery','template','bootstrap'],function($,template){
 	//调用接口获取所有的讲师数据
 	$.ajax({
 		type:'get',
 		url:'/api/teacher',
 		dataType:'json',
 		success:function(data){
-			console.log(data);
+			//console.log(data);
 			//解析数据渲染页面
 			var html = template('teacherTpl',{list:data.result});
 			$('#teacherInfo').html(html);
 			//启用注销功能，需要写在success中，等页面加载完事之后点击才生效
 			$('.eod').click(function(){
 				var that = this;
-				/*closest('td')获取最近的父元素*/
+				/*closest('td')获取最近的父元素类名是td*/
 				var td = $(this).closest('td');
 				/*console.log(td);*/
 				var tcID = td.attr('data-tcID');
@@ -32,6 +32,23 @@ define(['jquery','template'],function($,template){
 								$(that).text('启用');
 							}
 						}
+					}
+				});
+			});
+
+			//查看讲师
+			$('.preview').click(function(){
+				var td = $(this).closest('td');
+				var tcID = td.attr('data-tcID');
+				$.ajax({
+					type:'get',
+					url:'/api/teacher/view',
+					data:{tc_id:tcID},
+					dataType:'json',
+					success:function(data){
+						var html = template('modalTpl',data.result);
+						$('#modalInfo').html(html);
+						$('#teacherModal').modal();
 					}
 				})
 			})
