cc1e87c296dec7539e8c496ae7370c42c3f7fa41
tom <tom@itcast>
2017-09-22 09:58:37 +0800

    实现添加和编辑讲师页面共用

---
+++ public/js/util.js
@@ -0,0 +1,21 @@
+define(['jquery'],function($){
+	return {
+		qs : function(key){
+			//根据location的search方法，截取地址栏后面的参数，并且第一个问号不要
+			  var param = location.search.substr(1);
+			  var result = null;
+			  if(param){
+			    //有可能地址栏中有很多参数所以要按照&符号分割，然后遍历，
+			    var ps = param.split('&');
+			    $.each(ps,function(index,item){
+			      var kv = item.split('=');
+			      if(kv[0] == key){
+			        result =kv[1];
+			        return false; //终止each循环
+			      }
+			    })
+			  }
+			  return result;
+		}
+	}
+})
