50fd96bd3aa8891ca616a2a701f880906166d373
tom <tom@itcast>
2017-09-25 22:15:56 +0800

    完成课程基本信息提交功能

--- public/js/config.js
+++ public/js/config.js
@@ -22,7 +22,8 @@ require.config({
     index : '../js/index',
     courselist : '../js/course-list',
     courseadd : '../js/course-add',
-    coursebasic : '../js/course-basic'
+    coursebasic : '../js/course-basic',
+    coursepicture : '../js/course-picture'
   },
   shim:{
   	bootstrap:{
